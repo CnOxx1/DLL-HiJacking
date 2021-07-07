d8ec0fa88853ed75216dee858543a80f2eb49025
CnOxx1 <86923954+CnOxx1@users.noreply.github.com>
2021-07-07 16:43:05 +0800

    update WeChat_exe

---
+++ Wechat/Wechat_exe/WeChatWin_Dll/WeChatWin.cpp
@@ -0,0 +1,68 @@
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 头文件
+#include <Windows.h>
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 导出函数
+#pragma comment(linker, "/EXPORT:??0IChannelLogWriter@@QAE@$$QAV0@@Z=WeChatWin1Org.??0IChannelLogWriter@@QAE@$$QAV0@@Z,@1")
+#pragma comment(linker, "/EXPORT:??0IChannelLogWriter@@QAE@ABV0@@Z=WeChatWin1Org.??0IChannelLogWriter@@QAE@ABV0@@Z,@2")
+#pragma comment(linker, "/EXPORT:??0IChannelLogWriter@@QAE@XZ=WeChatWin1Org.??0IChannelLogWriter@@QAE@XZ,@3")
+#pragma comment(linker, "/EXPORT:??4IChannelLogWriter@@QAEAAV0@$$QAV0@@Z=WeChatWin1Org.??4IChannelLogWriter@@QAEAAV0@$$QAV0@@Z,@4")
+#pragma comment(linker, "/EXPORT:??4IChannelLogWriter@@QAEAAV0@ABV0@@Z=WeChatWin1Org.??4IChannelLogWriter@@QAEAAV0@ABV0@@Z,@5")
+#pragma comment(linker, "/EXPORT:??4ILogWriter@@QAEAAV0@$$QAV0@@Z=WeChatWin1Org.??4ILogWriter@@QAEAAV0@$$QAV0@@Z,@6")
+#pragma comment(linker, "/EXPORT:??4ILogWriter@@QAEAAV0@ABV0@@Z=WeChatWin1Org.??4ILogWriter@@QAEAAV0@ABV0@@Z,@7")
+#pragma comment(linker, "/EXPORT:??_7IChannelLogWriter@@6B@=WeChatWin1Org.??_7IChannelLogWriter@@6B@,@8")
+#pragma comment(linker, "/EXPORT:?AddExtraMem@TXBugReport@@YAHKI@Z=WeChatWin1Org.?AddExtraMem@TXBugReport@@YAHKI@Z,@9")
+#pragma comment(linker, "/EXPORT:?AddExtraMem@TXBugReport@@YAHPAXI@Z=WeChatWin1Org.?AddExtraMem@TXBugReport@@YAHPAXI@Z,@10")
+#pragma comment(linker, "/EXPORT:?AddIgnoreHookCheckModule@TXBugReport@@YAXPB_W@Z=WeChatWin1Org.?AddIgnoreHookCheckModule@TXBugReport@@YAXPB_W@Z,@11")
+#pragma comment(linker, "/EXPORT:?AddReleaseMonitorPoint@TXBugReport@@YAXPAJ@Z=WeChatWin1Org.?AddReleaseMonitorPoint@TXBugReport@@YAXPAJ@Z,@12")
+#pragma comment(linker, "/EXPORT:?DoBugReport@TXBugReport@@YAJPAU_EXCEPTION_POINTERS@@PB_W@Z=WeChatWin1Org.?DoBugReport@TXBugReport@@YAJPAU_EXCEPTION_POINTERS@@PB_W@Z,@13")
+#pragma comment(linker, "/EXPORT:?GetBugReportFlag@TXBugReport@@YAKXZ=WeChatWin1Org.?GetBugReportFlag@TXBugReport@@YAKXZ,@14")
+#pragma comment(linker, "/EXPORT:?GetBugReportInfo@TXBugReport@@YAPAUtagBugReportInfo@1@XZ=WeChatWin1Org.?GetBugReportInfo@TXBugReport@@YAPAUtagBugReportInfo@1@XZ,@15")
+#pragma comment(linker, "/EXPORT:?GetCustomFiltFunc@TXBugReport@@YAP6AHPAU_EXCEPTION_POINTERS@@@ZXZ=WeChatWin1Org.?GetCustomFiltFunc@TXBugReport@@YAP6AHPAU_EXCEPTION_POINTERS@@@ZXZ,@16")
+#pragma comment(linker, "/EXPORT:?InitBugReport@TXBugReport@@YAXPB_W000GGKHHKKP6GHPAUtagBugReportInfo@1@PBD200PAPAXPAKPAX@Z@Z=WeChatWin1Org.?InitBugReport@TXBugReport@@YAXPB_W000GGKHHKKP6GHPAUtagBugReportInfo@1@PBD200PAPAXPAKPAX@Z@Z,@17")
+#pragma comment(linker, "/EXPORT:?InitBugReportEx@TXBugReport@@YAXPB_W000GGKHHKKP6GHPAUtagBugReportInfo@1@PBD200PAPAXPAKPAX@ZH@Z=WeChatWin1Org.?InitBugReportEx@TXBugReport@@YAXPB_W000GGKHHKKP6GHPAUtagBugReportInfo@1@PBD200PAPAXPAKPAX@ZH@Z,@18")
+#pragma comment(linker, "/EXPORT:?RaiseSelfFatalException@TXBugReport@@YAXW4SelfException@1@@Z=WeChatWin1Org.?RaiseSelfFatalException@TXBugReport@@YAXW4SelfException@1@@Z,@19")
+#pragma comment(linker, "/EXPORT:?RecordCallStackIfNeed@TXBugReport@@YAXPAJ@Z=WeChatWin1Org.?RecordCallStackIfNeed@TXBugReport@@YAXPAJ@Z,@20")
+#pragma comment(linker, "/EXPORT:?SetBugReportFlag@TXBugReport@@YAHK@Z=WeChatWin1Org.?SetBugReportFlag@TXBugReport@@YAHK@Z,@21")
+#pragma comment(linker, "/EXPORT:?SetBugReportPath@TXBugReport@@YAHPB_W@Z=WeChatWin1Org.?SetBugReportPath@TXBugReport@@YAHPB_W@Z,@22")
+#pragma comment(linker, "/EXPORT:?SetBugReportUin@TXBugReport@@YAXKH@Z=WeChatWin1Org.?SetBugReportUin@TXBugReport@@YAXKH@Z,@23")
+#pragma comment(linker, "/EXPORT:?SetCustomFiltFunc@TXBugReport@@YAXP6AHPAU_EXCEPTION_POINTERS@@@Z@Z=WeChatWin1Org.?SetCustomFiltFunc@TXBugReport@@YAXP6AHPAU_EXCEPTION_POINTERS@@@Z@Z,@24")
+#pragma comment(linker, "/EXPORT:?SetExtInfo@TXBugReport@@YAHKKPB_W@Z=WeChatWin1Org.?SetExtInfo@TXBugReport@@YAHKKPB_W@Z,@25")
+#pragma comment(linker, "/EXPORT:?SetExtRptFilePath@TXBugReport@@YAHPB_W0@Z=WeChatWin1Org.?SetExtRptFilePath@TXBugReport@@YAHPB_W0@Z,@26")
+#pragma comment(linker, "/EXPORT:?SetLogFileMd5Dir@TXBugReport@@YAHPB_W00@Z=WeChatWin1Org.?SetLogFileMd5Dir@TXBugReport@@YAHPB_W00@Z,@27")
+#pragma comment(linker, "/EXPORT:?UninitBugReport@TXBugReport@@YAXXZ=WeChatWin1Org.?UninitBugReport@TXBugReport@@YAXXZ,@28")
+#pragma comment(linker, "/EXPORT:?ValidateBugReport@TXBugReport@@YAXXZ=WeChatWin1Org.?ValidateBugReport@TXBugReport@@YAXXZ,@29")
+#pragma comment(linker, "/EXPORT:?pfPostBugReport@TXBugReport@@3P6AXXZA=WeChatWin1Org.?pfPostBugReport@TXBugReport@@3P6AXXZA,@30")
+#pragma comment(linker, "/EXPORT:?pfPreBugReport@TXBugReport@@3P6AXXZA=WeChatWin1Org.?pfPreBugReport@TXBugReport@@3P6AXXZA,@31")
+#pragma comment(linker, "/EXPORT:GetHandleVerifier=WeChatWin1Org.GetHandleVerifier,@32")
+#pragma comment(linker, "/EXPORT:SignWith3Des=WeChatWin1Org.SignWith3Des,@33")
+#pragma comment(linker, "/EXPORT:StartWachat=WeChatWin1Org.StartWachat,@34")
+#pragma comment(linker, "/EXPORT:_TlsGetData@12=WeChatWin1Org._TlsGetData@12,@35")
+#pragma comment(linker, "/EXPORT:_TlsStoreData@12=WeChatWin1Org._TlsStoreData@12,@36")
+#pragma comment(linker, "/EXPORT:__ASSERT=WeChatWin1Org.__ASSERT,@37")
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 入口函数
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, PVOID pvReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(hModule);
+	}
+	else if (dwReason == DLL_PROCESS_DETACH)
+	{
+	}
+
+	return TRUE;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
